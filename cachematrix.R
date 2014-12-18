0271f530dfe75eeccec104e7c9f3fd2dbaf0776c
Diego <diego.villamil@konfio.mx>
2014-12-18 02:14:50 -0600

    Update cachematrix.R
    Just doing my homework.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Diego, 2014.
 
-## Write a short comment describing this function
+## MAKECACHEMATRIX and CACHESOLVE pair up together to make getting
+## the inverse of a matrix an efficient process.
 
-makeCacheMatrix <- function(x = matrix()) {
+## MAKECACHEMATRIX(X) initializes the matrix X by defining its
+## caching methods of 'setting' and 'getting' itself and its inverse.
+makeCacheMatrix <- function(X=matrix( )){
+    INV <- NULL
+    setMatrix  <- function(Y){
+        X   <<- Y
+        INV <<- NULL}
+    getMatrix  <- function( ){X}
+    setInverse <- function(I){
+        INV <<- I}
+    getInverse <- function( ){INV}
+    # Returns -or stores- its list of methods.
+    list(setMatrix=setMatrix,
+        getMatrix=getMatrix,
+        setInverse=setinverse,
+        getInverse=getinverse)}
 
-}
+## CACHESOLVE(X) gets the inverse of the matrix X by first checking
+## if it has been calculated already, or otherwise calculating it.
+cacheSolve <- function(X, ...){
+    INV <- X$getInverse( )
+    if(!is.null(INV)){
+        name <- deparse(substitute(X))
+        sentence <- paste("I found the inverse of %s in the ",
+                        "cache... getting it for you.")
+        message(sprintf(sentence, name))
+        return(INV)}
+    else{
+        X_  <- X$getMatrix( )
+        INV <- solve(X_, ...)
+        X$setInverse(INV)
+        return(INV)}}
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
