f41d3b50a4c78f661e3257a355d937222ef74be4
Diego <diego.villamil@konfio.mx>
2014-12-18 10:56:27 -0600

    Update cachematrix.R
    Changed notation for "super"-matrix XX.  I found it easier to understand as such.

--- cachematrix.R
+++ cachematrix.R
@@ -1,37 +1,41 @@
 ## Diego, 2014.
 
-## MAKECACHEMATRIX and CACHESOLVE pair up together to make getting
-## the inverse of a matrix an efficient process.
+## MAKECACHEMATRIX and CACHESOLVE pair up together to make getting the inverse of a matrix an efficient process.
 
-## MAKECACHEMATRIX(X) initializes the matrix X by defining its
-## caching methods of 'setting' and 'getting' itself and its inverse.
+## MAKECACHEMATRIX(X) initializes a "super"-matrix XX from the original X by defining its caching methods for 'setting' and 'getting' itself and its inverse.
 makeCacheMatrix <- function(X=matrix( )){
     INV <- NULL
-    setMatrix  <- function(Y){
+    setSelf    <- function(Y){
         X   <<- Y
         INV <<- NULL}
-    getMatrix  <- function( ){X}
+    getSelf    <- function( ){X}
     setInverse <- function(I){
         INV <<- I}
     getInverse <- function( ){INV}
-    # Returns -or stores- its list of methods.
-    list(setMatrix=setMatrix,
-        getMatrix=getMatrix,
-        setInverse=setinverse,
-        getInverse=getinverse)}
+    # The list of methods make X a "super"   XX.
+    return(list(setSelf=setSelf,
+                getSelf=getSelf,
+                setInverse=setInverse,
+                getInverse=getInverse))}
 
-## CACHESOLVE(X) gets the inverse of the matrix X by first checking
-## if it has been calculated already, or otherwise calculating it.
-cacheSolve <- function(X, ...){
-    INV <- X$getInverse( )
+
+## CACHESOLVE(XX) gets the inverse of the "super"-matrix XX, and hence of original X.  In order to do this, it checks if it has been calculated and cached, if not it then calculates and caches it.
+cacheSolve <- function(XX, ...){
+    INV <- XX$getInverse( )
     if(!is.null(INV)){
-        name <- deparse(substitute(X))
-        sentence <- "I found the inverse of %s in the cache... getting it for you."
-        message(sprintf(sentence, name))
+        name <- deparse(substitute(XX))
+        mssg <- paste("I found the inverse of %s in the ",
+            "cache... getting it for you.")
+        message(sprintf(mssg, name))
         return(INV)}
     else{
-        X_  <- X$getMatrix( )
-        INV <- solve(X_, ...)
-        X$setInverse(INV)
+        name <- deparse(substitute(XX))
+        mssg <- paste("I don't have the inverse of %s in the",
+            "cache... it might take a bit longer to calculate",
+            "it.")
+        message(sprintf(mssg, name))
+        X   <- XX$getSelf( )
+        INV <- solve(X, ...)
+        XX$setInverse(INV)
         return(INV)}}
 
