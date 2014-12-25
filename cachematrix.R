eee99ea70cfcafa3196990ceb4e8f404870774f7
Diego <diego.villamil@konfio.mx>
2014-12-25 14:12:17 -0600

    Update cachematrix.R
    Incorporate comments from peer review.

--- cachematrix.R
+++ cachematrix.R
@@ -1,42 +1,52 @@
 ## Diego, 2014.
 
-## MAKECACHEMATRIX and CACHESOLVE pair up together to make getting the inverse of a matrix an efficient process.
+## MAKECACHEMATRIX and CACHESOLVE pair up together to make getting 
+# the inverse of a matrix an efficient process.
 
-## MAKECACHEMATRIX(X) initializes a "super"-matrix XX from the "regular" matrix X by defining its caching methods for 'setting' and 'getting' itself and its inverse.
-makeCacheMatrix <- function(X=matrix( )){
-    INV <- NULL
-    setSelf    <- function(Y){
-        X   <<- Y
-        INV <<- NULL}
-    getSelf    <- function( ){X}
-    setInverse <- function(I){
-        INV <<- I}
-    getInverse <- function( ){INV}
-    # The list of methods make X a "super" XX.
-    return(list(setSelf=setSelf,
-                getSelf=getSelf,
-                setInverse=setInverse,
-                getInverse=getInverse))}
+## MAKECACHEMATRIX(X) initializes a cachematrix CMAT from the "regular" 
+# matrix MAT by defining its caching methods for 'setting' and 'getting' 
+# itself and its inverse.
+makeCacheMatrix <- function(mat=matrix( )){
+    inv <- NULL
+    
+    setSelf    <- function(m){
+        mat <<- m
+        inv <<- NULL}
+    getSelf    <- function( ){mat}
+    setInverse <- function(i){
+        inv <<- i}
+    getInverse <- function( ){inv}
+
+    # This list of methods is what make MAT a cache CMAT.
+    methods <- list(setSelf   =setSelf,    
+                    getSelf   =getSelf,
+                    setInverse=setInverse,
+                    getInverse=getInverse)
+    return(methods)}
 
 
-## CACHESOLVE(XX) gets the inverse of the "super"-matrix XX, and hence of original X.  In order to do this, it checks if it has been calculated and cached, if not it then calculates and caches it.
-cacheSolve <- function(XX, ...){    # Differentiate matrix X from "super"-matrix XX. 
-    inv <- XX$getInverse( )         # Keep in small letters, until it's verified to be the inverse.
-    if(!is.null(inv)){
-        name <- deparse(substitute(XX))     # Gets its name as a string for screen communication.  
-        mssg <- paste("I found the inverse of `%s` in the ",
+## CACHESOLVE(CMAT) gets the inverse of the cached-matrix CMAT, and 
+# hence of original MAT.  In order to do this, it checks if it has been 
+# calculated and cached, if not it then calculates and caches it.
+cacheSolve <- function(cmat, ...){ 
+    inv_ <- cmat$getInverse( )  # Check if INV_ is inverse.
+    if(!is.null(inv_)){
+        name   <- deparse(substitute(cmat))   # Variable name for communication purposes.
+        msgtmp <- paste("I found the inverse of `%s` in the ",
             "cache... getting it for ya.")
-        message(sprintf(mssg, name))
-        INV  <- inv                 # Upgrade the name into CAPS to return the inverse. 
-        return(INV)}
+        message(sprintf(msgtmp, name))
+        
+        inv  <- inv_            # INV_ being inverse confirmed.
+        return(inv)}
+    
     else{
-        name <- deparse(substitute(XX))
-        mssg <- paste("I don't have the inverse of `%s` in the",
-            "cache... it might take a bit longer to calculate",
-            "it.")
-        message(sprintf(mssg, name))
-        X   <- XX$getSelf( )        # Use library function SOLVE on "regular" matrix X to get inverse. 
-        INV <- solve(X, ...)
-        XX$setInverse(INV)
-        return(INV)}}
+        name   <- deparse(substitute(cmat))
+        msgtmp <- paste("I don't have the inverse of `%s` in the",
+            "cache... gonna calculate it for ya.")
+        message(sprintf(msgtmp, name))
+        
+        mat  <- cmat$getSelf( ) # Use library function SOLVE on matrix MAT to get inverse. 
+        inv  <- solve(mat, ...)
+        cmat$setInverse(inv)
+        return(inv)}}
 
